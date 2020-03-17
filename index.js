8f82b858adc127cdfa8f217e59b4f1815677fc07
Juan Miguel Garcia <juan-miguel.garcia@aboutyou.com>
2020-03-17 18:10:53 +0100

    [ADD] action creators -- new reducer

--- index.js
+++ index.js
@@ -27,22 +27,95 @@ function createStore(reducer) {
   };
 }
 // App code
+
+const ADD_TODO = "ADD_TODO";
+const REMOVE_TODO = "REMOVE_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const ADD_GOAL = "ADD_GOAL";
+const REMOVE_GOAL = "REMOVE_GOAL";
+const TOGGLE_GOAL = "TOGGLE_GOAL";
+
+const addTodoAction = todo => ({
+  type: ADD_TODO,
+  todo
+});
+
+const toggleTodoAction = id => ({
+  type: TOGGLE_TODO,
+  id
+});
+
+const removeTodoAction = id => ({
+  type: REMOVE_TODO,
+  id
+});
+
+const toggleGoalAction = id => ({
+  type: TOGGLE_GOAL,
+  id
+});
+
+const addGoalAction = goal => ({
+  type: ADD_GOAL,
+  goal
+});
+
+const removeGoalAction = id => ({
+  type: REMOVE_GOAL,
+  id
+});
+
 function todos(state = [], action) {
-  if (action.type === "ADD_TODO") {
-    return state.concat([action.todo]);
+  switch (action.type) {
+    case ADD_TODO:
+      return state.concat([action.todo]);
+    case TOGGLE_TODO:
+      return state.map(todo =>
+        todo.id !== action.id
+          ? todo
+          : Object.assign({}, todo, { complete: !todo.complete })
+      );
+    case REMOVE_TODO:
+      return state.filter(todo => todo !== action.id);
+    default:
+      return state;
   }
-  return state;
 }
 
-const store = createStore(todos);
+function goals(state = [], action) {
+  switch (action.type) {
+    case ADD_GOAL:
+      return state.concat([action.goal]);
+    case TOGGLE_GOAL:
+      return state.map(goal =>
+        goal.id !== action.id
+          ? goal
+          : Object.assign({}, goal, { complete: !goal.complete })
+      );
+    case REMOVE_GOAL:
+      return state.filter(goal => goal !== action.id);
+    default:
+      return state;
+  }
+}
+
+function app(state = {}, action) {
+  return {
+    goals: goals(state.goals, action),
+    todos: todos(state.todos, action)
+  };
+}
+
+const store = createStore(app);
 store.subscribe(() => {
   console.log("the new state is ", store.getState());
 });
-store.dispatch({
-  type: "ADD_TODOS",
-  todo: {
-    id: 0,
-    name: "learn redux",
-    complete: false
-  }
-});
+store.dispatch(
+  addTodoAction({
+    todo: {
+      id: 0,
+      name: "learn redux",
+      complete: false
+    }
+  })
+);
